b7c83ea5f335a600a7a3d3259c62fb15e6f1e489
The Beige Programmer <sylascuthill@gmail.com>
2017-11-27 17:31:02 -0500

    Revert "Revert "v2.1""
    This reverts commit 27d8a9cbd454e0b736125100233cc82177c01066.

--- Sync.js
+++ Sync.js
@@ -4,5 +4,13 @@ let Co = document.getElementById('c');
 let CodeData = document.getElementById('CodeData');
 let run = document.getElementById('run');
 
+Im.onchange = function() {
+	let file = Im.files[0];
+	reader = new FileReader();
+	reader.readAsText(file, "UTF-8");
+	console.log(reader);
+	c.innerText = reader.result;
+}
+
 run.onclick = function(){
 }
