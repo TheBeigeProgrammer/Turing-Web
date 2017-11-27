c4795da58bca3ffa043b86a03a19012662591ac4
Sylas Cuthill <32719015+TheBeigeProgrammer@users.noreply.github.com>
2017-11-27 10:07:57 -0500

    Add files via upload

---
+++ Sync.js
@@ -0,0 +1,8 @@
+let Im = document.getElementById('import');
+let Ou = document.getElementById('o');
+let Co = document.getElementById('c');
+let CodeData = document.getElementById('CodeData');
+let run = document.getElementById('run');
+
+run.onclick = function(){
+}
