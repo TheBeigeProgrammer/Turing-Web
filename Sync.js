65a11ecca63864c0fd1fd0271c33f52c0818388f
The Beige Programmer <sylascuthill@gmail.com>
2017-12-01 15:42:11 -0500

    v5.0.0
    Major CSS revamp:
    -Entire layout remake with css grid
    -Added IDE dive to house CodeData and Console
    -Revamped margins and padding with adaptive units
    
    Major html revamp:
    -Cleaned up the element selectors
    -Sorted elements into sub-divs
    -Added .grid and .IDE grid elements
    -Added a Clear button to clear CodeData

--- Sync.js
+++ Sync.js
@@ -1,10 +1,9 @@
 let Im = document.getElementById('import');
-let o = document.getElementById('o');
-let c = document.getElementById('c');
+let o = document.getElementById('ConsoleData');
+let c = document.getElementById('CodeData');
 let run = document.getElementById('run');
 let save = document.getElementById('save');
-let cls = document.getElementsById('clear')
-// let CodeData = "";
+let clear = document.getElementById('clear');
 Im.onchange = function() {
 	let file = Im.files[0];
 	reader = new FileReader();
@@ -13,32 +12,31 @@ Im.onchange = function() {
 		c.value = reader.result;
 	}
 }
-function saveTextAsFile(filename,data)
-{
-    var textToWrite = data;
-    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-    var fileNameToSaveAs = filename;
+function saveTextAsFile(filename,data){
+	var textToWrite = data;
+	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+	var fileNameToSaveAs = filename;
 
-    var downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "Download File";
-    if (window.webkitURL != null)
-    {
-        // Chrome allows the link to be clicked
-        // without actually adding it to the DOM.
-        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-    }
-    else
-    {
-        // Firefox requires the link to be added to the DOM
-        // before it can be clicked.
-        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-        downloadLink.onclick = destroyClickedElement;
-        downloadLink.style.display = "none";
-        document.body.appendChild(downloadLink);
-    }
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null)
+	{
+		// Chrome allows the link to be clicked
+		// without actually adding it to the DOM.
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	}
+	else
+	{
+		// Firefox requires the link to be added to the DOM
+		// before it can be clicked.
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
 
-    downloadLink.click();
+	downloadLink.click();
 }
 function passCodeToPy(Data){
 	//give data as variable to python script
@@ -47,9 +45,12 @@ function passCodeToPy(Data){
 
 run.onclick = function(){
 	// console.log("hey");
-		o.value = c.value.split(" ").reverse().toString().replace(/,/g," ")
+	o.value = c.value.split(" ").reverse().toString().replace(/,/g," ")
 }
 save.onclick = function(){
 	console.log(c.vale);
 	saveTextAsFile(window.prompt("file name"),c.value);
+}
+clear.onclick = function(){
+	c.value = "";
 }
